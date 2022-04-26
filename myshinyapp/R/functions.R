9962a5ebb516113336e66c5590feaa6879dcbbd5
cthiounn <45511529+cthiounn@users.noreply.github.com>
2022-04-26 11:39:12 +0200

    update graph dot observed

--- myshinyapp/R/functions.R
+++ myshinyapp/R/functions.R
@@ -43,7 +43,7 @@ plot_simulation_graph=function(simulation_dataframe){
   df_observed<- data.frame(x=time,y=infected,type="observed")
   df_simul <- data.frame(x=simulation_dataframe$time,y=simulation_dataframe$I,type="simulated")
   df<-rbind(df_observed,df_simul)
-  ggplot2::ggplot(data=df) + ggplot2::geom_line(mapping = ggplot2::aes(x , y ,colour = type)) + ggplot2::xlab("temps") + ggplot2::ylab("nombre d'infectés")
+  ggplot2::ggplot(data=df) + ggplot2::geom_line(data=df_simul,mapping = ggplot2::aes(x, y ,colour = type)) +ggplot2::geom_point(data=df_observed,mapping = ggplot2::aes(x, y ,colour = type))+ ggplot2::xlab("temps") +ggplot2::ylab("nombre d'infectés")
 } 
   
 calculer_vraisemblance=function(beta,gamma){
@@ -218,5 +218,5 @@ plot_simulation_graph_SIR_SEIR=function(simulation_dataframe_SIR,simulation_data
     df_simul_sir <- data.frame(x=simulation_dataframe_SIR$time,y=simulation_dataframe_SIR$I,type="simulated SIR")
     df_simul_seir <- data.frame(x=simulation_dataframe_SEIR$time,y=simulation_dataframe_SEIR$I,type="simulated SEIR")
     df<-rbind(df_observed,df_simul_sir,df_simul_seir)
-    ggplot2::ggplot(data=df) + ggplot2::geom_line(mapping = ggplot2::aes(x , y ,colour = type))  + ggplot2::xlab("temps") + ggplot2::ylab("nombre d'infectés")
+    ggplot2::ggplot(data=df)  + ggplot2::geom_line(data=df_simul_sir,mapping = ggplot2::aes(x, y ,colour = type)) + ggplot2::geom_line(data=df_simul_seir,mapping = ggplot2::aes(x, y ,colour = type)) +ggplot2::geom_point(data=df_observed,mapping = ggplot2::aes(x, y ,colour = type))+ ggplot2::xlab("temps") +ggplot2::ylab("nombre d'infectés")
 }
